1fe857be219cb15de1c202e2a7fde092be3c6313
zabiremu <zabirraihan570@gmail.com>
2024-02-23 20:45:56 +0600

    monsters search field completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            // eslint-disable-next-line no-lone-blocks
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    const filterMonstersData = this.state.monsters.filter((item) =>
+      item.name.toLowerCase().includes(this.state.searchField)
+    );
+    return (
+      <>
+        <div className="app">
+          <input
+            className="search-text"
+            name="search"
+            onChange={(event) => {
+              const searchField = event.target.value.toLowerCase();
+              this.setState(() => {
+                return { searchField };
+              });
+            }}
+          />
+          {filterMonstersData.map((monster) => {
+            return (
+              <>
+                <div key={monster.id}>
+                  <p>{monster.name}</p>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
